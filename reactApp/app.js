7e0db73ad16bde4101eba93d6d3eaf1162a0441f
Graham Smith <smitgw11@gmail.com>
2017-06-29 21:06:22 -0700

    configuration complete

---
+++ reactApp/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+ReactDOM.render(<p>React livesHHHH!</p>, document.getElementById('root'));
