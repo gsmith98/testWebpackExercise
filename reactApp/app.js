ca0a140b8bc06ba1a3b2127b662538553fdee636
Graham Smith <smitgw11@gmail.com>
2017-06-29 21:42:03 -0700

    Static Mockup TodoList

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// var React = require('react');
-// var ReactDOM = require('react-dom');
 
-ReactDOM.render(<p>React livesHHHH!</p>, document.getElementById('root'));
+const dummyData = ['Come to SF', 'Master React', 'Sleep'];
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {dummyData.map(task => <Todo task={task} />)}
+      </ul>
+    )
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li><button>X</button> {this.props.task}</li>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
